3c827a36bcaaf110d73c77a8e16c47ef8790910a
Guilherme Tavano <tavano@deco.cx>
2024-05-15 16:49:57 +0000

    Release staging environment with 3 changes made by Guilherme Tavano

--- loaders/cms.ts
+++ loaders/cms.ts
@@ -1,24 +1,28 @@
 import { allowCorsFor } from "deco/mod.ts";
 import { AppContext } from "../apps/site.ts";
 
-export interface Config {
-  text: string;
-  age: number;
-  showMenu: boolean;
+export interface WithTraffic<T> {
+  /**
+   * @maxItems 2
+   */
+  content: T[];
+  traffic?: number;
 }
 
 interface Props {
-
-  configs?: Config[];
+  text: WithTraffic<string>;
+  age: WithTraffic<number>;
+  showMenu: WithTraffic<boolean>
 }
 
 interface Returns {
-  configs?: Config[];
+  text: WithTraffic<string>;
+  age: WithTraffic<number>;
+  showMenu: WithTraffic<boolean>
 }
 
-
 export default function loader(
-  { configs }: Props,   
+  props: Props,   
   req: Request,
   ctx: AppContext
 ): Returns {
@@ -27,7 +31,5 @@ export default function loader(
     ctx.response.headers.set(name, value);
   });
 
-  return { 
-    configs,
-  }
+  return props;
 }
